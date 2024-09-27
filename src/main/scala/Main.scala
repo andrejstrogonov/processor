b33db129c43c32cce8efab748899e718087b4f59
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-27 20:47:27 +0300

    Third commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 package org.kiuru.processor
 
-import chisel3.{Bundle, Reg, UInt, Vec, VecInit, fromIntToLiteral, fromIntToWidth}
+import chisel3.{Bundle, Reg, UInt, Vec, VecInit, fromIntToWidth}
 
 class LPDCDecoder extends Bundle{
   val observation = Reg(Vec(14,UInt(32.W)))
@@ -8,9 +8,8 @@ class LPDCDecoder extends Bundle{
   val observationY = Reg(Vec(14,UInt(32.W)))
   val switchingMatrix = VecInit.fill(14,14)(32.W)
   val emissionMatrix = VecInit.fill(14,14)(32.W)
-  val randomVector = 
+  val randomVector = VecInit.fill(14,14)(32.W)
 
-  val multiply =
 }
 
 
