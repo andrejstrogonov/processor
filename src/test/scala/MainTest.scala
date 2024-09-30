87cea0e0ae1552d08708ec3eac1db8fc8fffd26d
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-30 20:22:39 +0300

    Add MainTest.scala

---
+++ src/test/scala/MainTest.scala
@@ -0,0 +1,9 @@
+package org.kiuru.processor
+
+import chiseltest.ChiselScalatestTester
+import org.scalatest.flatspec.AnyFlatSpec
+
+class MainTest extends AnyFlatSpec with ChiselScalatestTester  {
+
+
+}
