42084b76617e67067775b0ec244cd35603740aa9
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-13 11:38:55 +0300

    Add muliplexer 43:1 (25x18:1)

---
+++ src/main/scala/Multiplexer43to1Main.scala
@@ -0,0 +1,21 @@
+package org.kiuru.processor
+package org.kiuru.processor
+
+import chisel3._
+import circt.stage.ChiselStage
+
+class Multiplexer43to1 extends Module {
+  val io = IO(new Bundle {
+    val in = Input(Vec(43, UInt(1.W))) // 43 input lines, each 1 bit wide
+    val sel = Input(UInt(6.W))         // Selector signal, 6 bits to select one of 43 inputs
+    val out = Output(UInt(1.W))        // 1-bit output
+  })
+
+  // Use the selector to choose one of the inputs
+  io.out := io.in(io.sel)
+}
+
+// Generate the Verilog code
+object Multiplexer43to1Main extends App {
+  println(ChiselStage.emitSystemVerilog(new Multiplexer43to1))
+}
